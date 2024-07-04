8b01499fd57fa5c24d61334c0fd4718d5130103b
B-Rohan-Rao <raorohan060@gmail.com>
2024-07-04 16:49:48 +0530

    frontend completed

--- run.py
+++ run.py
@@ -11,25 +11,53 @@ secretkey = os.urandom(24)
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = secretkey
 
+def paginate(jobs):
+    jobs_per_page = 20
+    page = request.args.get('page', 1, type=int)
+    total_jobs = len(jobs['title'])
+    total_pages = (total_jobs + jobs_per_page - 1) // jobs_per_page
+    start = (page - 1) * jobs_per_page
+    end = start + jobs_per_page
+    paginated_jobs = {key: {k: v for k, v in value.items() if start <= k < end} for key, value in jobs.items()}
+
+    return paginated_jobs,page, total_pages, total_jobs
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/aisubmit',methods=['GET','POST'])
+@app.route("/aisubmit",methods=['GET','POST'])
 def aisubmit():
     if request.method == 'POST':
-        no_of_jobs = request.form['no_jobs']
-        return no_of_jobs
+        no_of_jobs = int(request.form['no_jobs'])
+        title = session.get('job')
+        skills = session.get('skillsInput')
+        salary = session.get('salaryInput')
 
+        df = pd.read_csv('job.csv')
+        dt = df.to_dict(orient='list')
+
+        order = similar(dt, title, skills, salary)
+        df = df.loc[df['index'].isin(order)].set_index('index').loc[order].reset_index()
+        jobs = df.to_dict()
+
+        jobs_per_page = min(no_of_jobs, 20)  # Display either the requested number of jobs or a maximum of 20 jobs per page
+        page = request.args.get('page', 1, type=int)
+        total_jobs = len(jobs['title'])
+        total_pages = (total_jobs + jobs_per_page - 1) // jobs_per_page
+
+        start = (page - 1) * jobs_per_page
+        end = start + jobs_per_page
+        paginated_jobs = {key: {k: v for k, v in value.items() if start <= k < end} for key, value in jobs.items()}
+
+        return render_template('job_list.html', jobs=paginated_jobs, page=page, total_pages=total_pages, data_fetched=True) 
 
 @app.route('/portal',methods=['GET','POST'])
 def portal():
-    if request.method == 'POST':
-        portl = request.form['jobportal']
-        session['jobportal'] = portl
-        return portl
-        
+        if request.method == 'POST':
+            portl = request.form['jobportal']
+            session['jobportal'] = portl
+            return portl
 
 @app.route("/job_list", methods=['GET', 'POST'])
 def job_list():
@@ -56,63 +84,21 @@ def job_list():
         df = df.sample(frac=1).reset_index(drop=True)
         jobs = {col: df[col].to_dict() for col in df.columns}
 
-        jobs_per_page = 20
-        page = request.args.get('page', 1, type=int)
-        total_jobs = len(jobs['title'])
-        total_pages = (total_jobs + jobs_per_page - 1) // jobs_per_page
-
-        start = (page - 1) * jobs_per_page
-        end = start + jobs_per_page
-        paginated_jobs = {key: {k: v for k, v in value.items() if start <= k < end} for key, value in jobs.items()}
+        paginated_jobs,page,total_pages,total_jobs = paginate(jobs)
 
         # Optionally return a success message or redirect to another page
         return render_template('job_list.html', jobs=paginated_jobs, page=page, total_pages=total_pages, total_jobs=total_jobs, data_fetched = data_fetched)
-    # return render_template('job_list.html')
-    prtl = session.get('jobportal')
-    print(prtl)
 
     file_path = r"job.csv"
     df = pd.read_csv(file_path)
-    num_jobs = len(df)
-    jobs = df.to_dict()
-    jobs_per_page = 20
-    page = request.args.get('page', 1, type=int)
-    total_jobs = len(jobs['title'])
-    total_pages = (total_jobs + jobs_per_page - 1) // jobs_per_page
+    df = df.sample(frac=1).reset_index(drop=True)
+    jobs = {col: df[col].to_dict() for col in df.columns}
 
-    start = (page - 1) * jobs_per_page
-    end = start + jobs_per_page
-    paginated_jobs = {key: {k: v for k, v in value.items() if start <= k < end} for key, value in jobs.items()}
+    jobs = df.to_dict()
+
+    paginated_jobs,page,total_pages,total_jobs = paginate(jobs)
 
     return render_template('job_list.html', jobs=paginated_jobs, page=page, total_pages=total_pages, total_jobs=total_jobs, data_fetched = data_fetched)
-
-@app.route("/rec", methods = ["POST", "GET"])
-def rec():
-    try:
-        title = session.get('job')
-        skills = session.get('skillsInput')
-        salary = session.get('salaryInput')
-
-        df =  pd.read_csv('job.csv')
-        dt = df.to_dict(orient='list')
-
-        order = similar(dt, title, skills, salary)
-        df = df.loc[df['index'].isin(order)].set_index('index').loc[order].reset_index()
-        jobs = df.to_dict()
-
-        jobs_per_page = 20
-        page = request.args.get('page', 1, type=int)
-        total_jobs = len(jobs['title'])
-        total_pages = (total_jobs + jobs_per_page - 1) // jobs_per_page
-
-        start = (page - 1) * jobs_per_page
-        end = start + jobs_per_page
-        paginated_jobs = {key: {k: v for k, v in value.items() if start <= k < end} for key, value in jobs.items()}
-
-        return render_template('job_list.html', jobs=paginated_jobs, page=page, total_pages=total_pages,data_fetched = True)
-    except:
-        flash("Error: Unable to perform this operation. Please try again later.")
-        return redirect(url_for('rec'))
 
 if __name__ == "__main__":
     app.run(debug=True)
